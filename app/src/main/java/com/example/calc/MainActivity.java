8164db0ce3e18c39fa9e63b2ca93badf850b87a6
Ayesha <noor2001114@gamil.com>
2023-03-30 22:35:10 +0500

    initial commit

---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.calc;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    ImageButton addbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addbtn= findViewById(R.id.addbtn);
+        addbtn.setOnClickListener((View.OnClickListener) this);
+    }
+    public void onClick (View v)
+    {
+        Intent switchAct = new Intent(MainActivity.this,additemtolist.class);
+        startActivity(switchAct);
+    }
+}
+
